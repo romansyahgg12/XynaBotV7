49ba66dd98bd46e673e50f9e8aff5cae45b28227
romansyahgg12345 <riziramadana@gmail.com>
2023-01-27 17:51:07 +0700

    first commit

---
+++ js/toanim.js
@@ -0,0 +1,115 @@
+const _0x51043d = _0xc559;
+(function (_0x3cc2d0, _0x54f9ad) {
+    const _0x921170 = _0xc559, _0x26dd87 = _0x3cc2d0();
+    while (!![]) {
+        try {
+            const _0x233f46 = -parseInt(_0x921170(0x1ed)) / (-0x2156 + -0x2463 + 0x45ba) * (-parseInt(_0x921170(0x1e4)) / (0x71 * 0x1b + -0x1286 + 0x69d)) + -parseInt(_0x921170(0x1ce)) / (-0x4 * -0x683 + -0x1 * 0x30e + -0x16fb) + -parseInt(_0x921170(0x1e2)) / (0xe8f * -0x1 + -0x1ae3 + 0x2976) * (-parseInt(_0x921170(0x1e1)) / (-0xe * 0x18d + 0x2 * -0x310 + 0x1bdb * 0x1)) + -parseInt(_0x921170(0x1dc)) / (-0x12 * -0x9c + 0x57 * -0x19 + -0x273) + parseInt(_0x921170(0x1d4)) / (-0x3c * 0x50 + 0x1438 + -0x171) + parseInt(_0x921170(0x1ca)) / (-0x6 * -0x637 + -0x65c * 0x5 + -0x2bb * 0x2) + -parseInt(_0x921170(0x1d6)) / (0x903 + 0x22b8 + -0x2bb2);
+            if (_0x233f46 === _0x54f9ad)
+                break;
+            else
+                _0x26dd87['push'](_0x26dd87['shift']());
+        } catch (_0x4b3dfb) {
+            _0x26dd87['push'](_0x26dd87['shift']());
+        }
+    }
+}(_0x4ee7, -0x3f6b6 + -0x7453f + 0x10c639 * 0x1));
+function _0xc559(_0x4ab6e7, _0x3b2a63) {
+    const _0x236559 = _0x4ee7();
+    return _0xc559 = function (_0xa67fdc, _0x4ad33c) {
+        _0xa67fdc = _0xa67fdc - (-0xd1f * 0x1 + -0x885 * 0x3 + 0x2877);
+        let _0x37cc01 = _0x236559[_0xa67fdc];
+        return _0x37cc01;
+    }, _0xc559(_0x4ab6e7, _0x3b2a63);
+}
+function _0x4ee7() {
+    const _0x24e355 = [
+        '3574368SpIqQz',
+        'JcRAa',
+        '2762199iyRORv',
+        'MZyts',
+        'extra',
+        'sendMessag',
+        'vRYvH',
+        'reply\x20the\x20',
+        '3981594aCSWvC',
+        '../lib/myf',
+        'split',
+        'nim.js',
+        'download',
+        '10wIdRtM',
+        '1288404cXrrag',
+        'Server\x20Tid',
+        '2afZmZN',
+        'exports',
+        'parse',
+        '0|1|4|2|5|',
+        'reply',
+        'img_urls',
+        'DknwV',
+        '../lib/toa',
+        'ace',
+        '279507wnhiLK',
+        'ng\x20\x0a\x0a',
+        '「\x20❗\x20」pleas',
+        'WXzqe',
+        '4332896nXefkl',
+        'unc',
+        'chat',
+        'NOmCv',
+        '1927077vbnslQ',
+        'log',
+        'img\x20with\x20f',
+        'ak\x20Menduku',
+        'process...',
+        'e\x20send\x20or\x20'
+    ];
+    _0x4ee7 = function () {
+        return _0x24e355;
+    };
+    return _0x4ee7();
+}
+const jadianime = require(_0x51043d(0x1eb) + _0x51043d(0x1df)), fs = require('fs'), {getBuffer, getRandom, sleep} = require(_0x51043d(0x1dd) + _0x51043d(0x1cb)), toAnime = async (_0x31d5e3, _0x5bf11e, _0x334336, _0x14ffa6, _0x4015f9) => {
+        const _0x546d07 = _0x51043d, _0x48b7c1 = {
+                'vRYvH': _0x546d07(0x1e7) + '3',
+                'JcRAa': _0x546d07(0x1ef) + _0x546d07(0x1d3) + _0x546d07(0x1db) + _0x546d07(0x1d0) + _0x546d07(0x1ec),
+                'MZyts': _0x546d07(0x1d2),
+                'DknwV': function (_0x283f7b, _0x31b863) {
+                    return _0x283f7b(_0x31b863);
+                },
+                'WXzqe': function (_0x4b6f06, _0xcba19d) {
+                    return _0x4b6f06 + _0xcba19d;
+                },
+                'NOmCv': _0x546d07(0x1e3) + _0x546d07(0x1d1) + _0x546d07(0x1ee)
+            };
+        try {
+            const _0xf9bf79 = _0x48b7c1[_0x546d07(0x1da)][_0x546d07(0x1de)]('|');
+            let _0xe3b7f2 = -0x122d + -0x1901 * 0x1 + 0x2b2e;
+            while (!![]) {
+                switch (_0xf9bf79[_0xe3b7f2++]) {
+                case '0':
+                    if (!_0x5bf11e)
+                        return _0x31d5e3[_0x546d07(0x1e8)](_0x48b7c1[_0x546d07(0x1d5)]);
+                    continue;
+                case '1':
+                    _0x31d5e3[_0x546d07(0x1e8)](_0x48b7c1[_0x546d07(0x1d7)]);
+                    continue;
+                case '2':
+                    var _0x447bff = await _0x48b7c1[_0x546d07(0x1ea)](jadianime, _0x3b292b);
+                    continue;
+                case '3':
+                    _0x4015f9[_0x546d07(0x1d9) + 'e'](_0x31d5e3[_0x546d07(0x1cc)], { 'image': { 'url': _0xfbff51 } }, { 'quoted': _0x334336 });
+                    continue;
+                case '4':
+                    var _0x3b292b = await _0x14ffa6[_0x546d07(0x1e0)]();
+                    continue;
+                case '5':
+                    var _0xfbff51 = JSON[_0x546d07(0x1e6)](_0x447bff[_0x546d07(0x1d8)])[_0x546d07(0x1e9)][-0x1813 + -0x479 * 0x5 + -0xb9c * -0x4];
+                    continue;
+                }
+                break;
+            }
+        } catch (_0x296bab) {
+            _0x31d5e3[_0x546d07(0x1e8)](_0x48b7c1[_0x546d07(0x1c9)](_0x48b7c1[_0x546d07(0x1cd)], _0x296bab)), console[_0x546d07(0x1cf)](_0x296bab);
+        }
+    };
+module[_0x51043d(0x1e5)] = toAnime;
